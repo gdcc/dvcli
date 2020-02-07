4e93e85d5bbd3a2e4af35f08e44aae56d5dadb75
Oliver Bertuch <o.bertuch@fz-juelich.de>
2020-02-07 22:35:43 +0100

    Add initial Click based CLI application installable with pip and creating shims

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dvcli',
+    version='0.0.1',
+    py_modules=find_packages(),
+    install_requires=[
+        'Click==7.0',
+        'pyDataverse==0.2.1'
+    ],
+    entry_points='''
+        [console_scripts]
+        dvcli=dvcli.cli:cli
+    ''',
+)
