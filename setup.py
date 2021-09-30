667e03eda620cb8d250e15c56f0a0fdf8cbd0eee
Oliver Bertuch <o.bertuch@fz-juelich.de>
2021-09-30 14:07:04 +0200

    chore(deps): update to latest Click etc

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='dvcli',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(),
     install_requires=[
-        'click==7.1.2',
+        'click==8.0.1',
         'click-plugins==1.1.1',
         'click-log==0.3.2',
-        'pyDataverse@git+https://github.com/AUSSDA/pyDataverse#3b040ff',
-        'confuse==1.0.0'
+        'pyDataverse==0.3.1',
+        'confuse==1.6.0'
     ],
     entry_points='''
         [console_scripts]
