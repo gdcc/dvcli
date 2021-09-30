5b687f35aacc19246d77b116cecf0410b4a619b9
Oliver Bertuch <o.bertuch@fz-juelich.de>
2021-09-30 14:14:53 +0200

    style: make main cli script flake8 compliant

--- dvcli/cli.py
+++ dvcli/cli.py
@@ -1,16 +1,22 @@
 from pkg_resources import iter_entry_points
 
-import click, confuse, sys, os
+import click
+import confuse
+import sys
+import os
 from click_plugins import with_plugins
 
-### Importing our base commands
-from .user import cli as user_commands
+# Importing commands from submodules
+import dvcli.user.cli as user_group
 
 configuration = confuse.LazyConfig('dvcli', __name__)
 
+
 @with_plugins(iter_entry_points('dvcli.plugins'))
 @click.group()
-@click.option('--config', help='custom path to config. Loading '+configuration.user_config_path()+' by default.', type=click.Path())
+@click.option('--config',
+              help='Custom path to config. Trying ' + configuration.user_config_path() + ' by default.',
+              type=click.Path())
 def cli(config):
     """
     Dataverse Command Line Interface.
@@ -24,11 +30,13 @@ def cli(config):
         try:
             configuration.set_file(config)
         except confuse.ConfigReadError:
-            click.echo('Error: could not read configuration file ('+os.path.abspath(config)+'). Cowardly aborting.', err=True)
+            click.echo('Error: could not read configuration file (' + os.path.abspath(config) + '). Cowardly aborting.',
+                       err=True)
             sys.exit(10)
 
     # pass other args from cmdline to configuration
     # example: configuration.set_args({'url': url})
+
 
 def main():
     """
@@ -36,8 +44,9 @@ def main():
     This function is called from the entrypoint script installed by setuptools.
     It enables using environment variables like DVCLI_CONFIG as options.
     """
-    user_commands.register(cli)
+    user_group.register(cli)
     cli(auto_envvar_prefix='DVCLI')
+
 
 # Allow running this file as standalone app without setuptools wrappers
 if __name__ == '__main__':
